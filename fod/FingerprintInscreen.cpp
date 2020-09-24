a94baf78ab24ca9eabe1b586c1b8d4ad75383887
Akash Srivastava <akashniki@gmail.com>
2020-09-24 22:31:13 +0200

    umi: fod: Update sensor size and position

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -34,9 +34,9 @@
 #define FOD_STATUS_ON 1
 #define FOD_STATUS_OFF 0
 
-#define FOD_SENSOR_X 455
-#define FOD_SENSOR_Y 1920
-#define FOD_SENSOR_SIZE 173
+#define FOD_SENSOR_X 441
+#define FOD_SENSOR_Y 1808
+#define FOD_SENSOR_SIZE 197
 
 namespace {
 
