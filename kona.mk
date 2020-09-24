6353ef467ca03ee52ecfb6178e5c00c0c1b219b3
Akash Srivastava <akashniki@gmail.com>
2020-09-24 10:28:40 +0200

    umi: Inherit vendor libraries

--- kona.mk
+++ kona.mk
@@ -23,6 +23,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Inherit proprietary libraries
+$(call inherit-product, vendor/xiaomi/umi/umi-vendor.mk)
+
 -include $(LOCAL_PATH)/product_prop.mk
 -include $(LOCAL_PATH)/system_prop.mk
 
