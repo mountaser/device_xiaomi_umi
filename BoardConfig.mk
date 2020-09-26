7896d71a72a22a2d5070fc04954a3a25cc01010d
Akito Mizukito <akito@evolution-x.org>
2020-09-26 13:54:50 +0000

    [TMP] Remove fod extension
    Change-Id: I2286da5b199ffb5996adcb9f186018ff7f842ae0
    Signed-off-by: Akito Mizukito <akito@evolution-x.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,9 +77,6 @@ endif
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
-# FOD
-TARGET_SURFACEFLINGER_FOD_LIB := //$(DEVICE_PATH):libfod_extension.raphael
-
 # HIDL
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 
